0df9a9ca496aa1f303a58767204035941d0e2dfd
Pedro Cunha <idk.pedroc@gmail.com>
2023-03-12 00:00:41 -0300

    Botão

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import customtkinter
+import customtkinter as ctk
 
-janela = customtkinter.CTk() #Criando a janela
+janela = ctk.CTk() #Criando a janela
 
 janela._set_appearance_mode("light") #Setando a aparência 
 janela.title("Wikipedia") #Setando o titulo da janela
@@ -8,5 +8,15 @@ janela.geometry("1280x700") #Setando o tamanho inicial da janela
 janela.maxsize(width=1280, height=700) #Setando o máximo que a janela pode expandir
 janela.minsize(width=1280, height=700)#Setando o mínimo que a janela pode expandir
 janela.resizable(width=False, height=False) #Pode configurar se a altura/largura é reconfiguravel
+
+
+btn_nova_tela = ctk.CTkButton(
+    janela, text="História dos Sistemas Operacionais",
+    border_width=0.5,
+    border_color="black",
+    fg_color = "transparent",
+    text_color="black",
+    hover=False
+    ).place(x=1, y=1)
 
 janela.mainloop() #Iniciando a janela
