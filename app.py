5cc221a77aea01920e92256d65045817758778d8
Pedro Cunha <idk.pedroc@gmail.com>
2023-03-11 23:34:14 -0300

    Configurações iniciais

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
-from tkinter import *
+import customtkinter
 
-janela = Tk()
+janela = customtkinter.CTk() #Criando a janela
+
+janela._set_appearance_mode("light") #Setando a aparência 
+janela.title("Wikipedia") #Setando o titulo da janela
+janela.geometry("1280x700") #Setando o tamanho inicial da janela
+janela.maxsize(width=1280, height=700) #Setando o máximo que a janela pode expandir
+janela.minsize(width=1280, height=700)#Setando o mínimo que a janela pode expandir
+janela.resizable(width=False, height=False) #Pode configurar se a altura/largura é reconfiguravel
+
+janela.mainloop() #Iniciando a janela
