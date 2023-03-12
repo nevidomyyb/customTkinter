47e50104efd29cd10699e783060c10decc974144
Pedro Cunha <idk.pedroc@gmail.com>
2023-03-12 02:24:47 -0300

    História dos SO

--- app.py
+++ app.py
@@ -1,22 +1,66 @@
 import customtkinter as ctk
-
+from mensagems import mensagem
 janela = ctk.CTk() #Criando a janela
 
 janela._set_appearance_mode("light") #Setando a aparência 
 janela.title("Wikipedia") #Setando o titulo da janela
 janela.geometry("1280x700") #Setando o tamanho inicial da janela
-janela.maxsize(width=1280, height=700) #Setando o máximo que a janela pode expandir
-janela.minsize(width=1280, height=700)#Setando o mínimo que a janela pode expandir
 janela.resizable(width=False, height=False) #Pode configurar se a altura/largura é reconfiguravel
 
+frame_header = ctk.CTkFrame(master=janela, width=1280, height=30, fg_color="#DCDAD9").place(x=1, y=1)
+frame_menu = ctk.CTkFrame(master=janela, width=280, height=700, fg_color="#DCDAD9").place(x=1, y=1)
 
-btn_nova_tela = ctk.CTkButton(
+historia_ativada = False
+funcionamento_ativada =  False
+def historia():
+    global mensagem  
+    global historia_ativada 
+    global funcionamento_ativada
+    funcionamento_ativada = False
+    if historia_ativada:
+        return
+    historia_ativada = True
+    textbox.delete("0.0", "end")
+    textbox.insert("0.0", mensagem)
+
+def funcionamento():
+    global funcionamento_ativada
+    global historia_ativada
+    historia_ativada = False
+    if funcionamento_ativada:
+        return
+    funcionamento_ativada = True
+    textbox.delete("0.0", "end")
+
+btn_1 = ctk.CTkButton(
     janela, text="História dos Sistemas Operacionais",
-    border_width=0.5,
+    border_width=0,
     border_color="black",
-    fg_color = "transparent",
+    fg_color = "#DCDAD9",
     text_color="black",
-    hover=False
-    ).place(x=1, y=1)
+    hover=True,
+    hover_color="#BFBFBF",
+    corner_radius=0,
+    command=historia
+    ).place(x=10, y=50)
+btn_2 = ctk.CTkButton(
+    janela, text="Funcionamento do SO",
+    border_width=0,
+    border_color="black",
+    fg_color = "#DCDAD9",
+    text_color="black",
+    hover=True,
+    hover_color="#BFBFBF",
+    corner_radius=0,
+    command=funcionamento
+    ).place(x=10, y=90)
+
+label = ctk.CTkLabel(master=janela, text="Wikipedia", fg_color="#DCDAD9")
+label.place(x=640, y=3)
+assuntos = ctk.CTkLabel(master=janela, text="Assuntos", fg_color="#DCDAD9")
+assuntos.place(x=100, y=3)
+
+textbox = ctk.CTkTextbox(janela, width=800, height=600)
+textbox.place(x=300, y=50)
 
 janela.mainloop() #Iniciando a janela
---
+++ mensagems.py
@@ -0,0 +1,43 @@
+mensagem = """                                                                                       História dos Sistemas Operacionais
+
+
+                                              					              INTRODUÇÃO
+
+Sistemas operacionais são ligados intimamente ligados à arquitetura dos computadores que são executados. 
+O primeiro computador digital foi projetado pelo matemático inglês Charles Babbage. Dedicou sua vida à construir uma máquina analítica, nunca a fez perfeitamente, pois na tecnologia de sua época não havia os aparatos necessários para tal feito.
+Ao decorrer de suas tentativas percebeu que precisaria de um “software” para sua máquina, e assim contratou Ada Lovelace, a primeira    programadora do mundo.
+
+                                                                                          Primeira Geração (1945-1955)
+				                           Válvulas e Painéis
+
+Em méados da década de 40 um grupo de estudiosos obtiveram sucesso na construção de máquinas de calcular. Eram mecânicas e         válvulas faziam todo o trabalho, eram enormes e lentas.
+A programação era feita baseada em ligações de fios e pelo manuseio de válvulas. Posteriormente na década de 50, cartões perfurados    eram utilizados para escrever programas.
+
+				                Segunda Geração (1955-1965)
+				              Transistores e sistemas de lote
+
+Essa geração foi revolucionária pela introdução dos usos de transistores, o que reduziu consideravelmente o tamanho de um computado, além de torná-los mais eficientes e confiáveis. Nessa geração também foi desenvolvido o sistema de lotes, o que permitia um computador executar múltiplas tarefas em sequência.
+
+				                Terceira Geração (1965-1980)
+					    CIs e multiprogramação
+
+Os circuitos integrados foram uma nova criação que diminuiu ainda mais o tamanho dos computadores, além de aumentarem a sua           eficiência.
+Nessa geração foram desenvolvidos vários novos sistemas e funcionalidades que facilitavam  o uso de um computador:
+* Multiprogramação
+* Tempo compartilhado
+* Spooling
+
+					           Quarta Geração 
+					   Computadores pessoais
+
+Os sistemas operacionais da quarta geração foram marcados por serem fáceis de usar e serem acessíveis, foram desenvolvidos para     funcionar em diversos tipos de hardwares.
+Essa geração também é marcada pela popularização de computadores pessoais, que podem ser usados em casa, foram projetados para 
+suportar interfaces gráficas, e oferecer recursos avançado.
+As interfaces gráficas de usuário são sistemas implementados na quarta geração que permite usuários comuns interajam com o 
+computador usando ícones, janelas e menus em vez de digitar comandos em um prompt.
+As inteterfaces gráficas (GUI) tornou os computadores mais acessíveis à usuários e permitiu a criação de aplicativos gráficos mais avançados.
+
+Fontes:
+* Sistemas Operacionais: projeto e implementação - Tanenbaum e Albert S. Woodhull
+
+"""
